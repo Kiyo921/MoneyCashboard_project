8e458e67dc187d2f0d5a28502109a02ff22ab02e
Kiyo <kiyo921@hotmail.co.jp>
2016-08-12 17:10:56 +0100

    Add empty CRUD in transaction_controller.rb

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -1,2 +1,33 @@
 require_relative('../models/transaction')
 
+
+#Index
+get "/transactions" do 
+
+end
+
+#New
+get "/transactions/new" do
+
+end
+
+#Create
+post "/transactions" do
+
+end
+
+#Show
+get "/transactions/show" do
+end
+
+#Edit
+get "/transactions/:id/edit" do
+end
+
+#Update
+post "/transactions/:id" do
+end
+
+#Delete
+post "/transactions" do
+end
