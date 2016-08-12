18324ea973ffb71f495251f10eae4667dc3b7aef
Kiyo <kiyo921@hotmail.co.jp>
2016-08-12 15:59:11 +0100

    Set up basics for library.sql

--- db/library.sql
+++ db/library.sql
@@ -1,6 +1,10 @@
+DROP TABLE tags
+DROP TABLE merchants
 DROP TABLE transactions
 DROP TABLE users
 
+
+### will develop once the basic function is finished
 CREATE TABLE users(
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
@@ -10,6 +14,20 @@ CREATE TABLE transactions(
   id SERIAL4 PRIMARY KEY,
   amount INT4,
   memo VARCHAR(255),
-  tag VARCHAR(255),
-  user_id INT4 references users(id) ON DELETE CASCADE
+  dates VARCHAR(255), # will change it to date type later
+  user_id INT4 REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE tags(
+  id SERIAL4 PRIMARY KEY,
+  tag_name VARCHAR(255),
+  transaction_id INT4 REFERENCES transactions(id) ON DELETE CASCADE
+);
+
+CREATE TABLE merchants (
+  id SERIAL4 PRIMARY KEY,
+  merchant_name VARCHAR(255),
+  transaction_id INT4 REFERENCES transactions(id) ON DELETE CASCADE
+);
+
+
