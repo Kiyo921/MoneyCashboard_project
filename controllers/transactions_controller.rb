521cbb436820d5aff63550a6f1397b3fb7f53e14
Kiyo <kiyo921@hotmail.co.jp>
2016-08-12 17:15:22 +0100

    Add empty CRUD in tags_controller.rb

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -1,1 +1,33 @@
-require_relative('../models/transaction')
+require_relative('../models/tag')
+
+
+#Index
+get "/tags" do 
+
+end
+
+#New
+get "/tags/new" do
+
+end
+
+#Create
+post "/tags" do
+
+end
+
+#Show
+get "/tags/show" do
+end
+
+#Edit
+get "/tags/:id/edit" do
+end
+
+#Update
+post "/tags/:id" do
+end
+
+#Delete
+post "/tags" do
+end
