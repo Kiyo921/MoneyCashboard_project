5440a924a8c117b463120c952f6eff986857727d
Kiyo <kiyo921@hotmail.co.jp>
2016-08-12 17:20:44 +0100

    Add empty CRUD functions in controllers.

---
+++ controllers/merchants_controller.rb
@@ -0,0 +1,33 @@
+require_relative('../models/merchant')
+
+
+#Index
+get "/merchants" do 
+
+end
+
+#New
+get "/merchants/new" do
+
+end
+
+#Create
+post "/merchants" do
+
+end
+
+#Show
+get "/merchants/show" do
+end
+
+#Edit
+get "/merchants/:id/edit" do
+end
+
+#Update
+post "/merchants/:id" do
+end
+
+#Delete
+post "/merchants" do
+end
---
+++ controllers/users_controller.rb
@@ -0,0 +1,33 @@
+require_relative('../models/user')
+
+
+#Index
+get "/users" do 
+
+end
+
+#New
+get "/users/new" do
+
+end
+
+#Create
+post "/users" do
+
+end
+
+#Show
+get "/users/show" do
+end
+
+#Edit
+get "/users/:id/edit" do
+end
+
+#Update
+post "/users/:id" do
+end
+
+#Delete
+post "/users" do
+end
