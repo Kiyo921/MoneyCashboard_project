699daf97ce9ff009c15c2f2adaa7ec341bef294a
Kiyo <kiyo921@hotmail.co.jp>
2016-08-12 21:35:42 +0100

    Add CRUD in tags and transactions controller

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -1,33 +1,45 @@
 require_relative('../models/merchant')
-
+require_relative('../models/transactions')
 
 #Index
 get '/merchants' do 
-
+  @merchants = Merchant.all()
+  erb(:'merchants/index')
 end
 
 #New
 get '/merchants/new' do
-
+  @transactions = Transaction.all()
+  erb(:'merchants/new')
 end
 
 #Create
 post '/merchants' do
-
+  @merchant = Merchant.new(params)
+  @merchant.save
+  redirect(to("/merchants"))
 end
 
 #Show
 get '/merchants/:id' do
+  @merchant = Merchant.find(params[:id])
+  erb(:'merchants/show')
 end
 
 #Edit
 get '/merchants/:id/edit' do
+  @merchant = Merchant.find(params[:id])
+  erb(:'merchants/edit')
 end
 
 #Update
 post '/merchants/:id' do
+  @merchant = Merchant.update(params)
+  redirect(to("/merchants/#{params[:id]}"))
 end
 
 #Delete
 post '/merchants/:id/delete' do
+  @merchant = Merchant.delete(params[:id])
+  redirect(to("/merchants"))
 end
--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -1,33 +1,46 @@
 require_relative('../models/tag')
+require_relative('../models/transactions')
 
 
 #Index
-get '/tags' do 
-
+get '/tags' do
+  @tags = Tag.all()
+  erb(:'tags/index')
 end
 
 #New
 get '/tags/new' do
-
+  @transactions = Transaction.all()
+  erb(:'tags/new')
 end
 
 #Create
 post '/tags' do
-
+  @tag = Tag.new(params)
+  @tag.save
+  redirect(to("/tags"))
 end
 
 #Show
 get '/tags/:id' do
+  @tag = Tag.find(params[:id])
+  erb(:'tags/show')
 end
 
 #Edit
 get '/tags/:id/edit' do
+  @tag = Tag.find(params[:id])
+  erb(:'tags/edit')
 end
 
 #Update
 post '/tags/:id' do
+  @tag = Tag.update(params)
+  redirect(to("/tags/#{params[:id]}"))
 end
 
 #Delete
 post '/tags/:id/delete' do
+  @tag = Tag.delete(params[:id])
+  redirect(to("/tags"))
 end
